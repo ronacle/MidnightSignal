5c0812750f2a35dba1a937779fe6b9d11c56b59f
ronacle <ronjc81@gmail.com>
2026-04-04 18:36:57 -0400

    8.6 why this signal intelligence

--- app/api/version/route.js
+++ app/api/version/route.js
@@ -2,7 +2,7 @@ export async function GET() {
   return Response.json({
     ok: true,
     app: "midnight-signal",
-    version: "8.5.0",
-    build: "since-last-visit-b",
+    version: "8.6.0",
+    build: "why-this-signal-b",
   });
 }
