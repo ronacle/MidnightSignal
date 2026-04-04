35bf7935254bd147a760f58523b73ac31884a74e
ronacle <ronjc81@gmail.com>
2026-04-03 21:34:50 -0400

    clean

--- app/about/page.tsx
+++
@@ -1,2 +0,0 @@
-import AboutPage from "../../components/AboutPage";
-export default function About() { return <AboutPage />; }
--- app/app/page.tsx
+++
@@ -1,3 +0,0 @@
-"use client";
-import Dashboard from "../../components/Dashboard";
-export default function AppPage() { return <Dashboard />; }
