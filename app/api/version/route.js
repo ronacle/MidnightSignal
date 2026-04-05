12ef11dcaf342875298e50c0f955dc013c96a1cf
ronacle <ronjc81@gmail.com>
2026-04-05 18:13:28 -0400

    11.7 micro refinement

--- app/api/version/route.js
+++ app/api/version/route.js
@@ -2,7 +2,7 @@ export async function GET() {
   return Response.json({
     ok: true,
     app: "midnight-signal",
-    version: "11.6",
-    build: "mobile-hero-reset-animated-beacon-return",
+    version: "11.7",
+    build: "micro-refinement",
   });
 }
