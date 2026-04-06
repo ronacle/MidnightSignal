ef9cf79896db4f2874427508541706c58e498592
ronacle <ronjc81@gmail.com>
2026-04-06 12:17:13 -0400

    reset

--- app/api/version/route.js
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  return Response.json({
-    ok: true,
-    app: "midnight-signal",
-    version: "11.11.0",
-    build: "polish-identity-lock",
-  });
-}
--- components/signals/Top20Grid.jsx
+++
@@ -1,8 +0,0 @@
-export default function Top20Grid() {
-  return (
-    <div>
-      <h2 className="text-lg">Top 20</h2>
-      {/* your existing asset cards */}
-    </div>
-  );
-}
